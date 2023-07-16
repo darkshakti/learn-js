0bc3fcd177186f6c4de47ff7300ed592ecb26f3e
Margarita Vladyko <m2.vladyko@qa-academy.by>
2023-07-17 00:14:18 +0300

    variables tasks done

---
+++ variables.js
@@ -0,0 +1,13 @@
+//Работа с переменными
+let admin, name;
+name = "Джон";
+admin = name;
+alert(admin);
+
+//Придумайте правильные имена
+let ourPlanetName = "Земля";
+let currentUserName;
+
+//Какие буквы (заглавные или строчные) использовать для имён констант?
+const BIRTHDAY = '18.04.1982';
+const age = calculateAge(BIRTHDAY);
